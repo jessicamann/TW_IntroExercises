04b064427d0dec5f33c03cb4448fec7a17425fcb
Jess Mann <jmann@smith.edu>
2015-06-07 00:28:14 -0400

    skeleton for prime factor exercise

---
+++ src/exercise9/Main.java
@@ -0,0 +1,17 @@
+package exercise9;
+
+import java.util.LinkedList;
+
+/**
+ * Created by jessicamann on 6/7/15.
+ * Prime factor list
+ */
+public class Main {
+    public static void main(String[] args){
+        generate(30);
+    }
+
+    private static LinkedList<Integer> generate(int n){
+        return null;
+    }
+}
