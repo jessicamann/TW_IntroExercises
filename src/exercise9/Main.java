46d1805a5107f9e89146345ec8c819b7436d7b69
Jess Mann <jmann@smith.edu>
2015-06-12 12:31:04 -0400

    complete ex9

--- src/exercise9/Main.java
+++ src/exercise9/Main.java
@@ -1,17 +1,28 @@
 package exercise9;
 
 import java.util.LinkedList;
+import java.util.Collections;
 
 /**
  * Created by jessicamann on 6/7/15.
  * Prime factor list
  */
 public class Main {
-    public static void main(String[] args){
-        generate(30);
-    }
+    public static void main(String[] args){generate(30);}
 
     private static LinkedList<Integer> generate(int n){
-        return null;
+        LinkedList<Integer> factors = new LinkedList<Integer>();
+        for(int i=2; i<=n; i++){
+            while(n % i == 0){
+                if(!factors.contains(i)){factors.add(i);}
+                n /= i;
+            }
+        }
+        Collections.sort(factors);
+        for(int num : factors){
+            System.out.print(num+" ");
+        }
+
+        return factors;
     }
 }
